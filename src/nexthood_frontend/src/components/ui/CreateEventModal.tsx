b5d64f089354e74af052891d830eb9347c97bd81
Donny C <chikwemdonald@gmail.com>
2024-11-12 09:10:48 +0100

    Added the events stuff

--- src/nexthood_frontend/src/components/ui/CreateEventModal.tsx
+++ src/nexthood_frontend/src/components/ui/CreateEventModal.tsx
@@ -19,22 +19,9 @@ const CreateEventModal = ({
           <div className="bg-white p-8 z-10  rounded-md shadow-md ">
             <p className="text-xl">Host your Next Event</p>
             <div className="mt-4">
-                <CreateEventForm />
+                <CreateEventForm setIsModalActive={setIsModalActive} handleSubmitEvent={handleSubmitEvent} />
             </div>
-            <div className="mt-4 flex justify-center items-center w-full gap-4">
-              <button
-                onClick={() => setIsModalActive(false)}
-                className="border border-red-500 text-red-500 w-1/2 rounded-md py-3 md:py-5 px-4"
-              >
-                Cancel
-              </button>
-              <button
-                onClick={handleSubmitEvent}
-                className="bg-primary text-white rounded-md w-1/2 py-3 md:py-5 px-4"
-              >
-                Create Event
-              </button>
-            </div>
+            
           </div>
         </div>
       )}
