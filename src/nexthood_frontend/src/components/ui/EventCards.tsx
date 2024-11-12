8a8b7d8bde4ec4a81d67601ed4316095e03fc426
code-stan <thecoderstanley@gmail.com>
2024-11-12 21:33:13 +0100

    changed h3 to h2

--- src/nexthood_frontend/src/components/ui/EventCards.tsx
+++ src/nexthood_frontend/src/components/ui/EventCards.tsx
@@ -4,7 +4,7 @@ const EventCards = ({ community, date, name }: Event) => {
   return (
     <div className="p-6 rounded-lg border border-black border-opacity-5 hover:shadow-lg">
       <div className="rounded-lg lg:w-[300px] h-[180px] bg-primary bg-opacity-5 mb-3"></div>
-      <h3>{name}</h3>
+      <h2>{name}</h2>
       <p>Hosted by: {community}</p>
       <p>Date: {date}</p>
     </div>
